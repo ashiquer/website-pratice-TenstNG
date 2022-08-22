c5fd5ec3acca119218540c3dbf84f3a07ac09c5f
ashiquer <48611059+ashiquer@users.noreply.github.com>
2022-08-23 01:17:09 +0600

    upload

---
+++ src/test/java/test/runner/LoginTestRunner.java
@@ -0,0 +1,34 @@
+package test.runner;
+
+import base.Setup;
+import pages.LoginPage;
+import utils.Utils;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTestRunner extends Setup {
+    LoginPage loginPage;
+    Utils utils;
+    @Test(priority = 2)
+    public void doLoginWithValidCreds() throws IOException, ParseException {
+        loginPage=new LoginPage(driver);
+        utils =new Utils();
+        utils.getUserCreds(utils.getUserCount());
+        //driver.get("http://automationpractice.com/");
+        boolean isLogoutFound= loginPage.doLoginWithValidCreds(utils.getEmail(),utils.getPassword());
+        Assert.assertTrue(isLogoutFound);
+        //loginPage.linkLogout.click();
+    }
+    @Test(priority = 1)
+    public void doLoginWithInvalidPass() throws IOException, ParseException {
+        driver.get("http://automationpractice.com/");
+        loginPage=new LoginPage(driver);
+        utils=new Utils();
+        utils.getUserCreds(1);
+        String validationMessage= loginPage.doLoginWithInvalidPass(utils.getEmail(),utils.getPassword());
+        Assert.assertTrue(validationMessage.contains("failed"));
+    }
+}
